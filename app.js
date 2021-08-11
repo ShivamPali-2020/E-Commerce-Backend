4fc45bd528cdfcb2c47a6a1aca1ac909878d6185
ShivamPali-2020 <pali.shivam2020@gmail.com>
2021-08-11 20:43:26 +0530

    Added environment Variable

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
+require('dotenv').config()
 const mongoose = require('mongoose');
 const express = require('express');
+
+
 const app= express();
 
-mongoose.connect("mongodb://localhost:27017/tshirt",{
+mongoose.connect(process.env.DATABASE,{
     useNewUrlParser: true, 
     useUnifiedTopology: true,
     useCreateIndex:true
 }).then(()=>{
     console.log("DB CONNECTED");
+}).catch(()=>{
+    console.log("DB GOT OOPSS")
 });
 
-const port=3000;
+const port= process.env.PORT || 3000;
 
 app.listen(port,()=>{
     console.log(`App is running at ${port}`);
