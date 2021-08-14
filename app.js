bab10b2f1463d12143dfad04bf0a640a0c6c6583
ShivamPali-2020 <pali.shivam2020@gmail.com>
2021-08-14 21:06:19 +0530

    Added Routes & Controller for Authentication signedout

--- app.js
+++ app.js
@@ -5,8 +5,12 @@ var bodyParser = require('body-parser');
 const cookieParser = require("cookie-parser");
 const cors= require("cors");
 
+//Importing routers
+const authRoute = require("./routes/auth")
+
 const app= express();
 
+//DB Connection
 mongoose.connect(process.env.DATABASE,{
     useNewUrlParser: true, 
     useUnifiedTopology: true,
@@ -17,13 +21,19 @@ mongoose.connect(process.env.DATABASE,{
     console.log("DB GOT OOPSS")
 });
 
+
+//Middleware
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(cors())
 
+//My Routes
+app.use("/api",authRoute);
 
+//PORT
 const port= process.env.PORT || 3000;
 
+//Starting a Server
 app.listen(port,()=>{
     console.log(`App is running at ${port}`);
 })
