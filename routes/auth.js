ac27f01688e253183ce94f047161d06b20d08bba
ShivamPali-2020 <pali.shivam2020@gmail.com>
2021-08-16 15:47:01 +0530

    Completed Authentication Part

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,30 @@
 var express = require("express");
 var router = express.Router();
-const { signout,signup } = require("../controllers/auth");
+const { signout,signup,signin, isSignedin } = require("../controllers/auth");
+const { check } = require('express-validator');
+const { min } = require("lodash");
 
-router.post("/signup", signup); 
+
+router.post("/signup",
+[
+    check('name').isLength({min:3}).withMessage('name must be at least 3 chars long'),
+    check('email').isEmail().withMessage('email is required'),
+    check('password').isLength({min:5}).withMessage('password must be at least 5 chars long')
+], signup); 
+
+router.post("/signin",
+[
+    check('email').isEmail().withMessage('email is required'),
+    check('password').isLength({min:5}).withMessage('password must be at least 5 chars long')
+], signin)
+
+router.get("/testroute",isSignedin,(req,res)=>{
+    res.send(
+        "Autherised User"
+    )
+});
+
+
 router.get("/signout", signout);
 
 module.exports = router;
